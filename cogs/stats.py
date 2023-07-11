164a7dcfc1b3c69458af38181652beb75a74b9dc
Pndablob <64608661+Pndablob@users.noreply.github.com>
2023-07-11 14:11:33 -0400

    fixed uptime command

--- cogs/stats.py
+++ cogs/stats.py
@@ -1,11 +1,12 @@
-from datetime import datetime, timedelta
+import math
+from datetime import datetime
 from cogs.utils.constants import bot_color
 
 from discord.ext import commands
 import discord
 
 
-class stats(commands.Cog):
+class Stats(commands.Cog):
     """displays some stats for the bot"""
 
     def __init__(self, bot):
@@ -18,17 +19,20 @@ class stats(commands.Cog):
 
     def get_uptime(self):
         print(datetime.utcnow())
-        return datetime.utcnow() - self.bot.start_time
+        return round(datetime.timestamp(datetime.utcnow()) - datetime.timestamp(self.bot.start_time))
 
     @commands.command(aliases=['ut'])
     async def uptime(self, ctx):
         """Shows how long the bot has been online for"""
-        await ctx.send(self.bot.start_time)
+        diff = self.get_uptime()  # seconds since startup
 
-        diff = self.get_uptime()
+        days = math.floor(diff / 86400)  # Floor of diff / sec-in-day (86400)
+        hours = math.floor((diff % 86400) / 3600)  # Floor of (diff mod sec-in-day) / sec-in-hour (3600)
+        minutes = math.floor((diff % 3600) / 60)  # Floor of (diff mod sec-in-hour) / sec-in-min (60)
+        seconds = (diff % 60)  # diff mod sec-in-min
 
-        embed = discord.Embed(title='<:online:876192917167964230> Uptime', color=bot_color, timestamp=datetime.utcnow(),
-                              description=f"`{diff.days}` days, ` `hours, ` ` minutes, `{diff.seconds}` seconds")
+        embed = discord.Embed(title='<:online:1127821209921400833> Uptime', color=bot_color, timestamp=datetime.utcnow(),
+                              description=f"`{days}` days, `{hours}`hours, `{minutes}` minutes, `{seconds}` seconds")
 
         await ctx.send(embed=embed)
 
@@ -39,4 +43,4 @@ class stats(commands.Cog):
 
 
 async def setup(bot):
-    await bot.add_cog(stats(bot=bot))
+    await bot.add_cog(Stats(bot=bot))
