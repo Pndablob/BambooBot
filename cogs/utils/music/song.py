c33237c3da4e551213b5d66ce7ba6c594cb23a8e
Pnda <64608661+Pndablob@users.noreply.github.com>
2024-01-05 22:02:48 -0500

    added song class to store song attributes

---
+++ cogs/utils/music/song.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from cogs.utils.constants import EMBED_COLOR
+
+from discord.ext import commands
+import discord
+
+
+class Song:
+    def __init__(self, song_url: str = None, uploader: str = None, title: str = None, duration: int = None, channel_url: str = None, thumbnail: str = None):
+        #self.info = self.SongInfo(song_url, uploader, title, duration, channel_url, thumbnail)
+
+    #class SongInfo:
+        #def __init__(self, song_url: str, uploader: str, title: str, duration: int, channel_url: str, thumbnail: str):
+        self.song_url = song_url
+        self.uploader = uploader
+        self.title = title
+        self.duration = duration
+        self.channel_url = channel_url
+        self.thumbnail = thumbnail
+
+    def format_embed(self) -> discord.Embed:
+        """formats the song info into a discord embed"""
+        embed = discord.Embed(title=f"Now playing", description=f"**[{self.title}]({self.song_url})**\nUploaded by **[{self.uploader}]({self.channel_url})**", color=EMBED_COLOR, timestamp=datetime.now())
+
+        dur = str(timedelta(seconds=self.duration)) if self.duration is not None else "unknown"
+        embed.add_field(name="Duration:", value=f"`{dur}`", inline=False)
+
+        if self.thumbnail is not None:
+            embed.set_thumbnail(url=self.thumbnail)
+
+        return embed
