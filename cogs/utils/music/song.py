3306a3c1b8f80bd0398af76c221d2dc11d79a99a
Pnda <64608661+Pndablob@users.noreply.github.com>
2024-01-07 02:50:31 -0500

    fixed song info embed

--- cogs/utils/music/song.py
+++ cogs/utils/music/song.py
@@ -6,12 +6,12 @@ import discord
 
 
 class Song:
-    def __init__(self, song_url: str = None, uploader: str = None, title: str = None, duration: int = None, channel_url: str = None, thumbnail: str = None):
+    def __init__(self, link: str = None, uploader: str = None, title: str = None, duration: int = None, channel_url: str = None, thumbnail: str = None):
         #self.info = self.SongInfo(song_url, uploader, title, duration, channel_url, thumbnail)
 
     #class SongInfo:
         #def __init__(self, song_url: str, uploader: str, title: str, duration: int, channel_url: str, thumbnail: str):
-        self.song_url = song_url
+        self.link = link
         self.uploader = uploader
         self.title = title
         self.duration = duration
@@ -20,7 +20,7 @@ class Song:
 
     def format_embed(self) -> discord.Embed:
         """formats the song info into a discord embed"""
-        embed = discord.Embed(title=f"Now playing", description=f"**[{self.title}]({self.song_url})**\nUploaded by **[{self.uploader}]({self.channel_url})**", color=EMBED_COLOR, timestamp=datetime.now())
+        embed = discord.Embed(title=f"Now playing", description=f"**[{self.title}]({self.link})**\nUploaded by **[{self.uploader}]({self.channel_url})**", color=EMBED_COLOR, timestamp=datetime.now())
 
         dur = str(timedelta(seconds=self.duration)) if self.duration is not None else "unknown"
         embed.add_field(name="Duration:", value=f"`{dur}`", inline=False)
